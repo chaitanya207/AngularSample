a410a8ebf2c180efcdf18bd8b60612557e7d4b0d
chaitanya1989 <bhuvanagirichaitanya@gmail.com>
2020-11-09 12:44:33 +0530

    viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { User } from 'src/assets/Models/user';
 import { Users } from '../assets/data.db';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-users [ngClass]="{'isActive':true,'isUser':true}" [ngStyle]="{'text-decoration':'underline'}"
+  <div #UserListParent>
+  <app-users #UserList [ngClass]="{'isActive':true,'isUser':true}" [ngStyle]="{'text-decoration':'underline'}"
   [style.background]="'red'"
   *ngFor="let user of users;index as i;first as isFirst;last as isLast;odd as isOdd;even as isEven"
   [class.isFirst]="isFirst"
   [class.isEven] = "isEven"
   (selectedUser)="onUserSelected($event)" 
   [user]="user" [userIndex]="i+1"></app-users>
+</div>
+  {{startDate | date:'MMM/dd/yyyy'}}
   `,
   styles: []
 })
@@ -20,8 +24,20 @@ export class AppComponent {
   users = Users;
   title = 'sampleApp';
 
+  // @ViewChild('UserList')
+  // user1:UsersComponent
+  @ViewChild('UserList',{read:ElementRef})
+  user1:ElementRef
+
+  @ViewChild('UserListParent')
+  userDetails:ElementRef
+
+  startDate = new Date();
+
   onUserSelected(user:User){
     console.log(user);
+    console.log(this.user1);
+    console.log(this.userDetails);
   }
 
 }
